99f4238d9f742427a8b067954419f53afacb6d3f
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2019-04-15 18:03:59 -0700

    JS is working now

--- javascript/javascript_labs/lab3/Lab3.js
+++ javascript/javascript_labs/lab3/Lab3.js
@@ -11,6 +11,35 @@ const updateDisplay = (value) => {
   valid.innerText = value
 }
 
+const validate = (card) => {
+  console.log('card input:',typeof(card), card)
+  let string = card.toString();
+  console.log('string input:',typeof(string), string)
+  const strings = string.split('').map(function(item) {
+    return parseInt(item, 10);
+    }
+  );
+
+  let checkDigit = strings.pop().toString();
+  let revCheckDigit = strings.reverse();
+
+  for (let i=0; i<revCheckDigit.length; i+=2) {
+    revCheckDigit[i] *= 2
+    if (revCheckDigit[i] >= 10){
+      revCheckDigit[i] -= 9;
+    }
+  }
+
+  let sum = revCheckDigit.reduce(add);
+  function add(accumulator, a) {
+      return accumulator + a;
+  }
+  console.log('sum input:',typeof(sum), sum)
+
+  let checker = sum.toString()
+  console.log('checker input:',typeof(checker), checker)
+  return (checker[1] === checkDigit ? 'Valid' : 'Not Valid')
+}
 
 // Default display text
 updateDisplay('345')
@@ -18,42 +47,9 @@ updateDisplay('345')
 
 //Event listener
 btn.addEventListener("click", (evt) =>{
-  let validate = (car) => {
-    console.log('car input:',typeof(car), car)
-    let string = car.toString();
-    console.log('string input:',typeof(string), string)
-    const strings = string.split('').map(function(item) {
-      return parseInt(item, 10);
-      }
-    );
-
-    let checkDigit = strings.pop().toString();
-    let revCheckDigit = strings.reverse();
-
-    for (let i=0; i<revCheckDigit.length; i+=2) {
-      revCheckDigit[i] *= 2
-      if (revCheckDigit[i] >= 10){
-        revCheckDigit[i] -= 9;
-      }
-    }
-
-    let sum = revCheckDigit.reduce(add);
-    function add(accumulator, a) {
-        return accumulator + a;
-    }
-    console.log('sum input:',typeof(sum), sum)
-
-    let checker = sum.toString()
-    console.log('checker input:',typeof(checker), checker)
-    if (checker[1] === checkDigit) {
-      return "Valid"
-    } else {
-      return "Not Valid"
-    };
-    updateDisplay('Is Valid?' +checker)
-
-  }
-})
+  const output = validate(card.value)
+  updateDisplay(output)
+ })
 
 
 // validate("4556737586899855")
